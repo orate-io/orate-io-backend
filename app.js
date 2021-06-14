dd73e08c5e32e464dc7d4998d707142f4dfb031f
Yacine-Saoudi <yac.sao453@gmail.com>
2021-06-14 14:35:12 -0400

    removed comments

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 
 const loginRouter = require('./controllers/login')
 const signupRouter = require('./controllers/signup')
-//middleware to be added in future
-// const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
+
 app.use(cors())
 app.use(express.json())
-app.use('/login', loginRouter) //params are the url and the controller
+app.use('/login', loginRouter)
 app.use('/signup', signupRouter)
-//add middleware and error handlers from middleware here
+
+app.use(middleware.unkownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,3 @@
-
-// logs all requests sent to the server with their path and content
 const reqLog = (request, response, next) => {
   console.log('Method: ', request.method)
   console.log('Path: ', request.path)
@@ -16,7 +14,7 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'error') {
     return response.status(400).send({ error: 'error name' })
-  } // template for error handler, to be expanded as more errors added
+  }
 
   next(error)
 }
