7f234f11c0439394c54dc37f3d4f8df720bf7d53
yacineabdelhamidsaoudi <yacine.abdelhamid.saoudi@gmail.com>
2021-06-19 13:06:04 -0400

    documentation of fucntions added documentation on config and middleware functions

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-
+/**
+ * PORT variable that holds the port being used for the backend.
+ * @param {string} PORT - port recieved from environmental variables.
+ */
 const PORT = process.env.PORT
 
 module.exports = {
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,14 +1,29 @@
+// const jwt = require('jsonwebtoken')
+
+/**
+ * log of information the user sends to the server.
+ * @log
+ * @param {string} method - method type of request.
+ * @param {string} path - url the rerquest is sent to.
+ * @param {string} body - content of the request.
+ */
 const reqLog = (request, response, next) => {
   console.log('Method: ', request.method)
   console.log('Path: ', request.path)
   console.log('Body: ', request.body)
   next()
 }
-
+/**
+ * json error message rerouter.
+ * @param {string} response - returns error message if url entered is unknown.
+ */
 const unkownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unkown endpoint' })
 }
-
+/**
+ * error handler middleware.
+ * @param {string} response - returns error message error is returned.
+ */
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
